b6b4843b13f16403dabe0141938bb648d11d981c
Elias Walder <elias.j.walder@student.uibk.ac.at>
2024-03-21 15:42:38 +0100

    LECON-54 changed overlay and added functions

--- src/main/java/com/lecture/coordinator/ui/controllers/UserDetailController.java
+++ src/main/java/com/lecture/coordinator/ui/controllers/UserDetailController.java
@@ -6,6 +6,7 @@ import com.lecture.coordinator.exceptions.user.UserRequiredFieldEmptyException;
 import com.lecture.coordinator.model.UserxRole;
 import com.lecture.coordinator.model.Userx;
 import com.lecture.coordinator.services.UserService;
+import org.apache.catalina.User;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -28,13 +30,8 @@ import java.util.Set;
 public class UserDetailController {
 
     private List<String> selectedRoles;
-
     private final UserService userService;
-
-    /**
-     * Attribute to cache the currently displayed user
-     */
-    private Userx user;
+    private Userx singleUser;
 
     public UserDetailController(UserService userService) {
         this.userService = userService;
@@ -42,60 +39,21 @@ public class UserDetailController {
 
     @PostConstruct
     public void init() {
-        this.user = null;
+        this.singleUser = null;
     }
 
-    /**
-     * Sets the currently displayed user and reloads it form db. This user is
-     * targeted by any further calls of
-     * {@link #doReloadUser()}, {@link #doSaveUser()} and
-     * {@link #doDeleteUser()}.
-     *
-     * @param user
-     */
-    public void setUser(Userx user) {
-        this.user = user;
-        doReloadUser();
-    }
-
-    /**
-     * Returns the currently displayed user.
-     *
-     * @return
-     */
-    public Userx getUser() {
-        return user;
-    }
-
-    /**
-     * Action to force a reload of the currently displayed user.
-     */
     public void doReloadUser() {
-        user = userService.loadUser(user.getUsername()).orElse(null);
+        this.singleUser = userService.loadUser(singleUser.getUsername()).orElse(null);
     }
 
-    /**
-     * Action to save the currently displayed user.
-     *
-     * @throws UserRequiredFieldEmptyException
-     * @throws UserAlreadyExistsException
-     */
     public void doSaveUser() throws UserAlreadyExistsException, UserRequiredFieldEmptyException, UserInvalidEmailException {
-        user = this.userService.saveUser(user);
+        this.singleUser = this.userService.saveUser(this.singleUser);
         init();
     }
 
-    /**
-     * Action to delete the currently displayed user.
-     */
-    public void doDeleteUser() {
-        this.userService.deleteUser(user);
-        user = null;
-        init();
-    }
 
     public void createUser() {
-        user = new Userx();
+        Userx user = new Userx();
 
         HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
                 .getRequest();
@@ -132,4 +90,13 @@ public class UserDetailController {
     public void setSelectedRoles(List<String> selectedRoles) {
         this.selectedRoles = selectedRoles;
     }
+
+    public Userx getSingleUser() {
+        return singleUser;
+    }
+
+    public void setSingleUser(Userx singleUser) {
+        this.singleUser = singleUser;
+    }
 }
+
--- src/main/java/com/lecture/coordinator/ui/controllers/UserListController.java
+++ src/main/java/com/lecture/coordinator/ui/controllers/UserListController.java
@@ -5,22 +5,34 @@ import com.lecture.coordinator.services.UserService;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-/**
- * Controller for the user list view.
- * <p>
- * This class is part of the skeleton project provided for students of the
- * course "Software Architecture" offered by Innsbruck University.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 @Scope("view")
 public class UserListController {
     protected final UserService userService;
 
+    List<Userx> selectedUsers;
+
     public UserListController(UserService userService) {
         this.userService = userService;
+        this.selectedUsers = new ArrayList<>();
     }
 
     public Iterable<Userx> getUsers() {
         return userService.getAllUsers();
+    }
+
+    public List<Userx> getSelectedUsers() {
+        return selectedUsers;
+    }
+
+    public void setSelectedUsers(List<Userx> selectedUsers) {
+        this.selectedUsers = selectedUsers;
+    }
+
+    public Userx getFirstUser(){
+        return selectedUsers.get(0);
     }
 }
