585590776f1e886fdbea1ef77ffa975502f95e3d
Elias Walder CSAZ8844 <elias.j.walder@student.uibk.ac.at>
2024-05-21 13:15:25 +0200

    LECON-68 fixed colors

--- Frontend/src/app/components/wizzard/wizard.component.ts
+++ Frontend/src/app/components/wizzard/wizard.component.ts
@@ -27,4 +27,28 @@ export class WizardComponent {
   option9: boolean | undefined = false;
 
   option10: boolean | undefined = false;
+
+  getColorBasedOnIndex(type: string, index: number): string {
+    console.log(type + " : " + index);
+    if (index > this.active) {
+      return '#CDCDCC';
+    }
+
+    if (type === 'i') {
+      switch (index) {
+        case 0:
+          return '#9EC252';
+        case 1:
+          return '#75CF84';
+        case 2:
+          return '#75CFCA';
+        case 3:
+          return '#75A9CF';
+        default:
+          return '#CDCDCC';
+      }
+    } else {
+      return '#070707';
+    }
+  }
 }
