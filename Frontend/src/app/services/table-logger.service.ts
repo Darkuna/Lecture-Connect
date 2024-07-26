c70682cd8bf56a9b396ca1f431cc76c9358c172d
Elias Walder <elias.j.walder@student.uibk.ac.at>
2024-07-26 13:36:50 +0200

    LECON-112 added logger

--- Frontend/src/app/services/table-logger.service.ts
+++ Frontend/src/app/services/table-logger.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import {LoginUserInfoService} from "./login-user-info.service";
+import {TableLogDto} from "../../assets/Models/dto/table-log-dto";
+import LocalTime from "ts-time/LocalTime";
+import {LoggerType} from "../../assets/Models/enums/logger-types";
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +12,27 @@ export class TableLoggerService {
     private userInfoService: LoginUserInfoService
   ) { }
 
+  create(){
+    return new TableLogDto(Date.now().toString(), LoggerType.CREATE, undefined, this.userInfoService.username);
+  }
 
+  add(){
+    return new TableLogDto(Date.now().toString(), LoggerType.ADD, undefined, this.userInfoService.username);
+  }
+
+  update(){
+    return new TableLogDto(Date.now().toString(), LoggerType.UPDATE, undefined, this.userInfoService.username);
+  }
+
+  delete(){
+    return new TableLogDto(Date.now().toString(), LoggerType.DELETE, undefined, this.userInfoService.username);
+  }
+
+  schedule(){
+    return new TableLogDto(Date.now().toString(), LoggerType.SCHEDULE, undefined, this.userInfoService.username);
+  }
+
+  status(){
+    return new TableLogDto(Date.now().toString(), LoggerType.STATUS, undefined, this.userInfoService.username);
+  }
 }
--- Frontend/src/assets/Models/dto/table-log-dto.ts
+++ Frontend/src/assets/Models/dto/table-log-dto.ts
@@ -4,8 +4,15 @@ import {Room} from "../room";
 
 export class TableLogDto {
   eventDate?: string; //due to converting problems
-  text?: string;
   logType!: LoggerType;
-  logObject?: Room | Course;
+  logObject: Room | Course | undefined;
   username!: string;
+
+
+  constructor(eventDate: string, logType: LoggerType, logObject: Room | Course | undefined, username: string) {
+    this.eventDate = eventDate;
+    this.logType = logType;
+    this.logObject = logObject;
+    this.username = username;
+  }
 }
