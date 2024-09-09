c8fca414eb6e9faee3edbabd13b79e74dfab434f
Elias Walder <elias.j.walder@student.uibk.ac.at>
2024-09-09 12:42:31 +0200

    LECON-133 added card for wizard

--- Frontend/src/app/components/wizzard/1-course-selection/data-wizard/data-wizard.component.ts
+++ Frontend/src/app/components/wizzard/1-course-selection/data-wizard/data-wizard.component.ts
@@ -1,18 +1,64 @@
-import {Component, Input, Output} from '@angular/core';
+import {Component, Input, OnInit, Output} from '@angular/core';
 import {CourseDTO} from "../../../../../assets/Models/dto/course-dto";
 import {TmpTimeTable} from "../../../../../assets/Models/tmp-time-table";
+import {n} from "@fullcalendar/core/internal-common";
+
+
+export interface PageContent {
+  header?: string;
+  text?: string;
+  buttons?: {
+    text: string;
+    color: string;
+  }[];
+}
+
+
 
 @Component({
   selector: 'app-data-wizard',
   templateUrl: './data-wizard.component.html',
   styleUrl: './data-wizard.component.css'
 })
-export class DataWizardComponent {
+export class DataWizardComponent{
   @Input() tmpTimeTable!: TmpTimeTable;
   @Input() allCourses: CourseDTO[] = [];
   @Output() initialSelection: CourseDTO[] = [];
 
-  inputData(){
+  pageContent!: [PageContent, PageContent];
+  isFirstSectionConfirmed = true;
 
+ constructor() {
+   this.pageContent = [
+     {
+       header: 'Course Degree(s)', text:'Select the degree of courses',
+       buttons:[
+         {text:'yes', color:'#889A3E'},
+         {text:'no',  color:'#AD5353'}
+       ]
+     },
+     {
+       header: 'Semester Courses', text:'Do you want to only include the courses of the selected semester?',
+       buttons:[
+         {text:'Bachelor Computer Science',   color:'var(--system-color-primary-orange)'},
+         {text:'Master Computer Science',     color:'var(--system-color-primary-orange)'},
+         {text:'Master Software Engineering', color:'var(--system-color-primary-orange)'}
+       ]
+     }
+   ]
+ }
+
+ showButton(val: any){
+   console.log('showButton', val);
+ }
+
+  handleFirstSectionClick(buttonText: string) {
+    if (buttonText === 'yes') {
+      this.isFirstSectionConfirmed = true;
+    }
   }
+  inputData(){
+  }
+
+
 }
