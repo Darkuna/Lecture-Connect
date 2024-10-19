26079372ee05286e5c37ea046c0ea3bb884021dc
DarkSeaTuna <elias.walder@pfeifergroup.com>
2024-10-19 11:35:59 +0200

    added tailwind

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Include all your Angular component files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
