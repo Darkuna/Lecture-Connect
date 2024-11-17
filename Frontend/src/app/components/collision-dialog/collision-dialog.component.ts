527cd8d51ecb6813f112b1906d593f938edc15d9
Darkuna <elias.j.walder@student.uibk.ac.at>
2024-11-17 22:19:20 +0100

    finished collision preview

--- Frontend/src/app/components/collision-dialog/collision-dialog.component.ts
+++ Frontend/src/app/components/collision-dialog/collision-dialog.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CollisionService} from "../../services/collision.service";
-import {CollisionType} from "../../../assets/Models/enums/collision-type";
+import {CollisionType, getCollisionTypes} from "../../../assets/Models/enums/collision-type";
 
 @Component({
   selector: 'app-collision-dialog',
   templateUrl: './collision-dialog.component.html',
   styleUrl: './collision-dialog.component.css'
 })
-export class CollisionDialogComponent {
+export class CollisionDialogComponent implements OnInit{
+  cols: any[] = [];
+
   constructor(
     protected collisionService: CollisionService
   ) {}
 
+  ngOnInit(): void {
+    this.cols = [
+      {field: 'key', header: 'Course'},
+      {field: 'value', header: 'Type'},
+    ];
+  }
+
   getAllCollisions(): Record<string, CollisionType[]>{
     return this.collisionService.getAllCollisions();
   }
+
+  protected readonly getCollisionTypes = getCollisionTypes;
 }
