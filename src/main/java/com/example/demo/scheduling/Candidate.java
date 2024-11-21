59cd6f3e130a458f13d16618c2b931f7e523c663
Johannes Karrer <johannes.karrer@student.uibk.ac.at>
2024-11-21 20:40:44 +0100

    finished documentation of the algorithm

--- src/main/java/com/example/demo/scheduling/Candidate.java
+++ src/main/java/com/example/demo/scheduling/Candidate.java
@@ -23,22 +23,44 @@ public class Candidate {
         this.preferredSlots = preferredSlots;
     }
 
+    /**
+     * This method checks if two candidate's timings are intersecting
+     * @param candidate to be compared with
+     * @return true if the candidates are intersecting, false if not
+     */
     public boolean intersects(Candidate candidate) {
         return AvailabilityMatrix.toTiming(this).intersects(AvailabilityMatrix.toTiming(candidate));
     }
 
+    /**
+     * Checks if two candidates are in the same room
+     * @param candidate to be compared with
+     * @return true if the candidates are from the same room
+     */
     public boolean isInSameRoom(Candidate candidate) {
         return this.availabilityMatrix.getRoomTable().equals(candidate.getAvailabilityMatrix().getRoomTable());
     }
 
+    /**
+     * Checks if two candidates are from the same day
+     * @param candidate to be compared with
+     * @return true if their day attribute has the same value, false if not
+     */
     public boolean hasSameDay(Candidate candidate) {
         return this.day == candidate.getDay();
     }
 
+    /**
+     * This method assigns a courseSession to the candidate's availability matrix
+     * @param courseSession to be assigned
+     */
     public void assignToCourseSession(CourseSession courseSession){
         availabilityMatrix.assignCourseSession(this, courseSession);
     }
 
+    /**
+     * This method clears the candidate in the availability matrix.
+     */
     public void clearInAvailabilityMatrix(){
         availabilityMatrix.clearCandidate(this);
     }
@@ -51,6 +73,10 @@ public class Candidate {
         return String.format("Candidate %s, Day: %d, Slot: %d", availabilityMatrix.getRoomTable().getRoomId(), day, slot);
     }
 
+    /**
+     * This method calculates and return the end slot of the candidate
+     * @return slot index of end slot
+     */
     public int getEndSlot(){
         return slot + duration / 15 - 1;
     }
--- src/main/java/com/example/demo/scheduling/CollisionType.java
+++ src/main/java/com/example/demo/scheduling/CollisionType.java
@@ -1,5 +1,9 @@
 package com.example.demo.scheduling;
 
+/**
+ * This enum class is used in the collision check to give feedback about the different kinds of collisions a courseSession
+ * can have.
+ */
 public enum CollisionType {
     ROOM_CAPACITY,
     COURSE_TIMING_CONSTRAINTS,
