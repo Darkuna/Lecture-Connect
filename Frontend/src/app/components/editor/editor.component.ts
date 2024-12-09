1dbd302625b01494d69be3858f074628fc3077cf
Darkuna <elias.j.walder@student.uibk.ac.at>
2024-12-09 23:00:03 +0100

    minor css editor changes

--- Frontend/src/app/components/editor/editor.component.ts
+++ Frontend/src/app/components/editor/editor.component.ts
@@ -208,15 +208,15 @@ export class EditorComponent implements AfterViewInit, OnDestroy{
 
     const session = this.findSession()
     this.items.push(
-      { label: session!.fixed ? 'free Course' : 'fix Course', icon: 'pi pi-copy', command: () => { this.changeSessionBlockState() }},
-      { label: 'unassign Course', icon: 'pi pi-copy', command: () => { this.unassignCourse() } },
-      { label: 'remove Group', icon: 'pi pi-file-edit', command: ()=> { this.deleteCourse()} }
+      { label: session!.fixed ? 'free Course' : 'fix Course', icon: session!.fixed ? 'pi pi-unlock':'pi pi-lock', command: () => { this.changeSessionBlockState() }},
+      { label: 'unassign Course', icon: 'pi pi-reply', command: () => { this.unassignCourse() } },
+      { label: 'remove Group', icon: 'pi pi-delete-left', command: ()=> { this.deleteCourse()} }
     )
 
     const tmp = session!.name.slice(0, 2);
     this.items.push((tmp == 'PS' || tmp == 'SL') ?
-      { label: 'add Group', icon: 'pi pi-file-edit', command: ()=> { this.addCourseWithPsCharacter() } }
-        : { label: 'split Course', icon: 'pi pi-file-edit', disabled: true }
+      { label: 'add Group', icon: 'pi pi-plus-circle', command: ()=> { this.addCourseWithPsCharacter() } }
+        : { label: 'split Course', icon: 'pi pi-arrow-up-right-and-arrow-down-left-from-center', disabled: true }
       )
   }
 
