9a84aab5e703ea7ac818d074fefa9d41b77c27c5
Johannes Karrer <johannes.karrer@student.uibk.ac.at>
2024-12-13 11:28:16 +0100

    finished unit tests for CourseSessionService

--- src/main/java/at/uibk/leco/services/TimingService.java
+++ src/main/java/at/uibk/leco/services/TimingService.java
@@ -69,6 +69,9 @@ public class TimingService {
      */
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
     public Timing createTiming(Timing timing){
+        if(timing == null){
+            return null;
+        }
         timing.setId(null);
         if(timing.getStartTime().isAfter(timing.getEndTime())){
             throw new IllegalArgumentException("startTime must be before endTime");
