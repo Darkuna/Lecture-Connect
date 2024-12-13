cf6655bcfea8b53f44692ef4b2b99d3754d5825b
Johannes Karrer <johannes.karrer@student.uibk.ac.at>
2024-12-13 16:28:21 +0100

    added unit test for GlobalTableChangeService

---
+++ src/test/java/at/uibk/leco/service/GlobalTableChangeServiceTest.java
@@ -0,0 +1,41 @@
+package at.uibk.leco.service;
+
+import at.uibk.leco.models.TimeTable;
+import at.uibk.leco.models.enums.ChangeType;
+import at.uibk.leco.models.GlobalTableChange;
+import at.uibk.leco.services.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@WebAppConfiguration
+@ActiveProfiles("dev")
+public class GlobalTableChangeServiceTest {
+    @Autowired
+    private TimeTableService timeTableService;
+    @Autowired
+    private GlobalTableChangeService globalTableChangeService;
+
+    @Test
+    @WithMockUser(username = "user1", authorities = {"USER"})
+    public void testCreateGlobalTableChangeAndLoad() {
+        TimeTable timeTable = timeTableService.loadTimeTable(-1);
+
+        List<GlobalTableChange> changes = globalTableChangeService.loadAllByTimeTable(timeTable);
+
+        assertEquals(0, changes.size());
+
+        globalTableChangeService.create(ChangeType.ASSIGN_COURSE, timeTable, "Test description");
+        changes = globalTableChangeService.loadAllByTimeTable(timeTable);
+
+        assertEquals(1, changes.size());
+    }
+}
